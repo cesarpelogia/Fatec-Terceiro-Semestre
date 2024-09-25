6162155d744ea376028ceff7ad5334702c4bcbba
Cesar Pelogia <pelogia.cesar@hotmail.com>
2024-09-24 22:22:22 -0300

    Adicionando Pilha com cabeça

---
+++ Estrutura de Dados/Pilha/pilha/src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        StackWithoutHead stackWithoutHead = new StackWithoutHead();
+
+        stackWithoutHead.addNode(0);
+        stackWithoutHead.addNode(1);
+        stackWithoutHead.addNode(2);
+
+        stackWithoutHead.printStack();
+
+        stackWithoutHead.removeNode();
+
+        stackWithoutHead.printStack();
+
+
+    }
+}
---
+++ Estrutura de Dados/Pilha/pilha/src/StackWithoutHead.java
@@ -0,0 +1,38 @@
+public class StackWithoutHead {
+
+    private Node top;
+
+    public void addNode(int value){
+
+        Node newNode = new Node(value);
+
+        if (top == null){
+            top = newNode;
+            return;
+        }
+        newNode.setNext(top);
+        top = newNode;
+    }
+
+    public Node removeNode(){
+        if(top == null){
+            System.out.println("A Pilha está vazia.");
+            return null;
+        }
+        Node nodeToRemove = top;
+        top = top.getNext();
+        return nodeToRemove;
+    }
+
+    public void printStack(){
+        if(top == null){
+            System.err.println("Pilha vazia.");
+            return;
+        }
+        Node current = top;
+        while (current != null) {
+            System.out.println(current.getValue());
+            current= current.getNext();
+        }
+    }
+}
