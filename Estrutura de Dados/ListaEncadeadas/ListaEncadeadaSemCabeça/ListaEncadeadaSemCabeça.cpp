997a982daab0e2b8587ae10a23739fdd774426f6
Cesar Pelogia <pelogia.cesar@hotmail.com>
2024-09-30 17:34:41 -0300

    Atualizado lista sem cabeça

--- Estrutura de Dados/ListaEncadeadas/ListaEncadeadaSemCabeça/ListaEncadeadaSemCabeça.cpp
+++ Estrutura de Dados/ListaEncadeadas/ListaEncadeadaSemCabeça/ListaEncadeadaSemCabeça.cpp
@@ -9,133 +9,38 @@ struct Node {
     Node(string name, int value) : name(name), value(value), next(nullptr) {}
 };
 
-Node* head = nullptr;
+struct LinkedListWhitoutHead {
+private:
+    Node* head;
 
+public:
+    LinkedListWhitoutHead() : head(nullptr) {}
 
-struct LinkedListWhitoutHead{
-
-    void addNode(string name, int value){
-        Node* newNode = new Node(name, value);
-        int counter = 0;
-        
-        if(head == nullptr){
-            head = newNode;
+    void initializeList(string name, int value) {
+        if (head == nullptr) {
+            head = new Node(name, value);
+            cout << "Lista inicializada com nó de nome: " << head->name << " e valor: " << head->value << endl << endl;
         } else {
-            Node* temporary = head;
-            counter++;
-            while (temporary->next != nullptr){
-                counter++;
-                temporary = temporary->next;
-            }
-            temporary->next = newNode;
+            cout << "A lista já foi inicializada." << endl << endl;
         }
-        cout << "Nó adicionado na posição " << counter << ", com nome: " << newNode->name << " e valor: " << newNode->value << endl << endl;
-    };
-
-    void getNodeByPosition(int position){
-        Node* current = head;
-        int counter = 0;
-
-        while (counter != position){
-            if(current == nullptr){
-                cout << "Posição não encontrada" <<  endl << endl;
-                break;
-            }
-            counter++;
-            current = current->next;
-            if(counter == position){
-            cout << "Nó encontrado! Nome: " << current->name << " Valor: " << current->value <<  endl << endl;
-                break;
-            }
-        }
-    };
-
-    void updateNode(string olderName, int olderValue, string newName, int newValue) {
-        Node* current = head;
-        int counter = 0;
-
-        while (current != nullptr) {
-            if (current->name == olderName) {
-                if (current->value != olderValue) {
-                    cout << "Parâmetros incorretos. Nome e valor não coincidem" <<  endl << endl;
-                    return;
-                } else {
-                    current->name = newName;
-                    current->value = newValue;
-                    cout << "Nó atualizado na posição " << counter << ". Atualizado para: " << current->name << ", " << current->value <<  endl << endl;
-                    return;
-                }
-            }
-            current = current->next;
-            counter++;
-        }
-        cout << "Nó não encontrado" <<  endl << endl;
     }
 
+    void addNode(string name, int value) {
+        Node** headRef = &head;
+        Node* newNode = new Node(name, value);
+        newNode->next = *headRef;
+        *headRef = newNode;
 
-    void deleteNode(string name, int value){
-        Node* current = head;
-        Node* previous = nullptr;
-        int counter = 0;
-
-        if(head == nullptr){
-            cout << "A lista está vazia" <<  endl << endl;
-            return;
-        }
-        
-        if (head->name == name && head->value == value){
-            Node* temporary = head;
-            head = head->next;
-            delete temporary;
-            cout << "Nó removido na posição " << counter <<  endl << endl;
-            return;
-        }
-
-        previous = head;
-        current = head->next;
-        counter++;
-        
-        while(current != nullptr){
-            if (current->name == name){
-                if (current->value != value){
-                    cout << "Parâmetros incorretos. Nome e valor não coincidem" <<  endl << endl;
-                    return;
-                } else{
-                    previous->next = current->next;
-                    delete current;
-                    cout << "Nó deletado na posição " << counter<<  endl << endl;
-                    return;
-                }
-            }
-
-            previous = current;
-            current = current->next;
-            counter++;
-        }
-        cout << "Nó não encontrado" << endl << endl;
+        cout << "Nó adicionado com nome: " << newNode->name << " e valor: " << newNode->value << endl << endl;
     }
 };
 
-int main(){
-
+int main() {
     LinkedListWhitoutHead lst;
 
-    lst.addNode("Anália", 0);
-    lst.addNode("Bento", 1);
-    lst.addNode("Carlos", 2);
-    lst.addNode("Diego", 3);
-
-    lst.getNodeByPosition(2); // Deve encontrar Carlos
-
-    lst.updateNode("Carlos", 2, "Carlos Atualizado", 22); // Deve atualizar Carlos
-
-    lst.getNodeByPosition(2); // Deve encontrar Carlos Atualizado
-
-    lst.deleteNode("Carlos Atualizado", 22); // Deve deletar Carlos Atualizado
-
-    lst.getNodeByPosition(2); // Deve encontrar Diego
-
-
+    lst.initializeList("Node1", 1);
+    lst.addNode("Node2", 2);
+    lst.addNode("Node3", 3);
 
     return 0;
-};
+}
