8bbbee7aa415777e1f7659240d2c709f83648256
Cesar Pelogia <pelogia.cesar@hotmail.com>
2024-11-09 16:56:56 -0300

    Inserindo funções de impressão

--- Estrutura de Dados/Árvore/Tree.cpp
+++ Estrutura de Dados/Árvore/Tree.cpp
@@ -19,18 +19,63 @@ struct Tree {
         root = insertNode(root, val);
     }
 
+        void printInOrder() {
+        cout << "Percurso In-Order (Em Ordem):" << endl;
+        inOrder(root);
+        cout << endl;
+    }
+
+    void printPreOrder() {
+        cout << "Percurso Pre-Order (Pré-Ordem):" << endl;
+        preOrder(root);
+        cout << endl;
+    }
+
+    void printPostOrder() {
+        cout << "Percurso Post-Order (Pós-Ordem):" << endl;
+        postOrder(root);
+        cout << endl;
+    }
+
 private:
     Node* insertNode(Node* node, int val) {
         if (node == nullptr) {
+            cout << "Inserindo nó com valor " << val << endl;
             return new Node(val);
         }
 
         if (val < node->value) {
+            cout << "Valor " << val << " é menor que " << node->value << ", indo para a esquerda." << endl;
             node->left = insertNode(node->left, val);
-        } else {
+        } else if (val > node->value){
+            cout << "Valor " << val << " é maior que " << node->value << ", indo para a direita." << endl;
             node->right = insertNode(node->right, val);
         }
         return node;
+    }
+
+    void inOrder(Node* node){
+        if (node != nullptr){
+            inOrder(node->left);
+            cout << "Visitando nó com valor: " << node->value << endl;
+            inOrder(node->right);
+        }
+    }
+
+    void preOrder(Node* node) {
+        if (node != nullptr) {
+            cout << "Visitando nó com valor: " << node->value << endl;
+            preOrder(node->left);
+            preOrder(node->right);
+        }
+    }
+
+    void postOrder(Node* node) {
+        if (node != nullptr) {
+            postOrder(node->left);
+            postOrder(node->right);
+            cout << "Visitando nó com valor: " << node->value << endl;
+        }
     }
 };
 
@@ -44,5 +89,9 @@ int main(){
         tree.insert(val);
     }
     
+    tree.printInOrder();
+    tree.printPreOrder();
+    tree.printPostOrder();
+
     return 0;
 }
