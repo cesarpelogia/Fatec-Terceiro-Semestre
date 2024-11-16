c3e5e1a13c799b6adbcf92e183547b32e3278f7d
Cesar Pelogia <pelogia.cesar@hotmail.com>
2024-11-16 15:03:01 -0300

    Adicionado função para criar o banco caso não exista

--- Laboratório de Desenvolvimento em Banco de Dados/atividade2/cerveja/src/main/java/com/cervejaria/cerveja/CervejaApplication.java
+++ Laboratório de Desenvolvimento em Banco de Dados/atividade2/cerveja/src/main/java/com/cervejaria/cerveja/CervejaApplication.java
@@ -1,13 +1,33 @@
 package com.cervejaria.cerveja;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CervejaApplication {
 
+    @Autowired
+    private DataSource dataSource;
+
     public static void main(String[] args) {
         SpringApplication.run(CervejaApplication.class, args);
+    }
 
+    @PostConstruct
+    public void init() {
+        createDatabaseIfNotExists();
+    }
+
+    private void createDatabaseIfNotExists() {
+        try (var connection = dataSource.getConnection()) {
+            var statement = connection.createStatement();
+            statement.executeUpdate("CREATE DATABASE IF NOT EXISTS cervejaria");
+            statement.executeUpdate("USE cervejaria");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
