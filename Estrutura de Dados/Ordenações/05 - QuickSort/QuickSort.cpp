dbb9cf1fcd69f42f65d51a41fd849fffb48cc302
Cesar Pelogia <pelogia.cesar@hotmail.com>
2024-12-02 17:15:51 -0300

    Adicionado QuickSort

--- Estrutura de Dados/Ordenações/05 - QuickSort/QuickSort.cpp
+++ Estrutura de Dados/Ordenações/05 - QuickSort/QuickSort.cpp
@@ -2,3 +2,48 @@
 #include <vector>
 using namespace std;
 
+int partition(vector<int>& vec, int low, int high) {
+    int pivot = vec[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+
+        if (vec[j] < pivot) {
+            i++;
+            swap(vec[i], vec[j]);
+        }
+    }
+    
+    swap(vec[i + 1], vec[high]);
+    return (i + 1);
+}
+
+void quickSort(vector<int>& vec, int low, int high) {
+    
+    
+    if (low < high) {
+        int pi = partition(vec, low, high);
+
+        quickSort(vec, low, pi - 1);
+        quickSort(vec, pi + 1, high);
+    }
+}
+
+int main() {
+
+    vector<int> vec = {10, 7, 8, 9, 1, 5};
+
+    int n = vec.size();
+
+    quickSort(vec, 0, n - 1);
+
+    cout << "Array ordenado: ";
+
+    for (int i = 0; i < n; i++) {
+        cout << vec[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
